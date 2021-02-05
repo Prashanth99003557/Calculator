c0123ef788c38a693fe810bd646850027e29a894
Mokshith B <78561696+99003560@users.noreply.github.com>
2021-02-05 18:33:24 +0530

    Update calculator_operations.c

--- 4_Implementation/src/calculator_operations.c
+++ 4_Implementation/src/calculator_operations.c
@@ -23,6 +23,31 @@ int divide(int operand1, int operand2)
         return operand1 / operand2;
 }
 
+int greater(int operand1, int operand2)
+{
+    if(operand1 > operand2)
+        return operand1;
+    else
+        return operand2;
+}
+
+int smaller(int operand1, int operand2)
+{
+    if(operand1 < operand2)
+        return operand1;
+    else
+        return operand2;
+}
+
+int rectarea(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int rectperi(int operand1, int operand2)
+{
+    return (2*(operand1 + operand2));
+}
 
 int prime(int operand1)
 {
@@ -83,7 +108,111 @@ int or(int operand1, int operand2)
 {
     int result;
     if(operand1<0 && operand2<0)
-    return -1;int subtractor3bit(int operand1, int operand2, int operand3)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 1;
+    else if(operand1==0 && operand2==0)
+    return 0;
+    else
+    return 1;
+}
+
+int not(int operand1)
+{
+    if(operand1==0)
+        return 1;
+    else
+        return 0;
+}
+int nand(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==1 && operand2==1)
+    return 0;
+    else
+    return 1;
+}
+int nor(int operand1, int operand2)
+{
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==0 && operand2==0)
+    return 1;
+    else
+    return 0;
+}
+int factorial(int operand1)
+    {   
+        int fact=1;
+        if (operand1==0)
+        {
+            return 1;
+        }
+        if (operand1 < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 1; i <= operand1; ++i)
+            {
+                fact *= i;
+            }
+        return fact;
+        }
+    }
+int adder3bit(int operand1, int operand2, int operand3)
+
+{
+    if(operand1 < 0 || operand2 < 0 || operand3 < 0)
+          {
+             return -1;
+          }
+     else if (operand1 > 1 || operand2 > 1 || operand3 > 1)
+          {
+              return -1;
+           }
+     else
+        {
+           if(operand1==0 && operand2==0 && operand3==0)
+              {
+               return 0;
+              }
+           else if(operand1==0 && operand2==0 && operand3==1)
+              {
+               return 1;
+              }
+            else if(operand1==0 && operand2==1 && operand3==0)
+               {
+               return 1;
+               }
+        else if(operand1==0 && operand2==1 && operand3==1)
+              {
+               return 0;
+               }
+       else if(operand1==1 && operand2==0 && operand3==0)
+               {
+               return 1;
+               }
+       else if(operand1==1 && operand2==0 && operand3==1)
+              {
+               return 0;
+               }
+       else if(operand1==1 && operand2==1 && operand3==0)
+               {
+               return 0;
+               }
+        else 
+              return 1;
+     }
+ }
+int subtractor3bit(int operand1, int operand2, int operand3)
 {
     if(operand1 < 0 || operand2 < 0 || operand3 < 0)
     {
@@ -128,62 +257,7 @@ int or(int operand1, int operand2)
             return 1;
         }
     }
-    else if(operand1>1 && operand2>1)
-    return 1;
-    else if(operand1==0 && operand2==0)
-    return 0;
-    else
-    return 1;
 }
 
-int not(int operand1)
-{
-    if(operand1==0)
-        return 1;
-    else
-        return 0;
-}
-int nand(int operand1, int operand2)
-{
-    int result;
-    if(operand1<0 && operand2<0)
-    return -1;
-    else if(operand1>1 && operand2>1)
-    return 0;
-    else if(operand1==1 && operand2==1)
-    return 0;
-    elsevoid test_prime(void) {
-  CU_ASSERT(0 == prime(6));
-  CU_ASSERT(1 == prime(3));
-  
-  /* Dummy fail*/
-  CU_ASSERT(0 == prime(3));
-}
-    else if(operand1==0 && operand2==0)
-    return 1;
-    else
-    return 0;
-}
 
-int factorial(int operand1)
-    {   
-        int fact=1;
-        if (operand1==0)
-        {
-            return 1;
-        }
-        if (operand1 < 0)
-        {
-            return 0;
-        }
-        else
-        {
-            for (int i = 1; i <= operand1; ++i)
-            {
-                fact *= i;
-            }
-        return fact;
-        }
-    }
-    
-    
+
